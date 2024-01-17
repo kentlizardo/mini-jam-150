894f8f2015376ae9e923e08d86e0778dfc95c40a
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-17 03:15:39 -0600

    Created starter Godot Project

---
+++ scenes/util/debug.gd
@@ -0,0 +1,30 @@
+class_name Debug
+## Static class used as a helper library for debug purposes.
+##
+## These functions are not optimized for performance, 
+## but for readability in case of future modification.
+##
+
+# TODO: When method overloading is added, consolidate the following.
+# Or keep it the same for more type safety?
+static func obj_info(obj: Object) -> String:
+	return obj.get_script().get_path() + "(%s)" % obj.get_class() if obj.get_script() != null else obj.get_class()
+static func node_info(node: Node) -> String:
+	return obj_info(node)
+static func res_info(res: Resource) -> String:
+	return obj_info(res)
+
+const TREE_INDENT = "\t"
+static func _tree_info(node: Node) -> String:
+	return (node.name + ": %s" % node_info(node))
+static func _dump_tree_helper(x: Node, level : int) -> String:
+	var info := TREE_INDENT.repeat(level) + _tree_info(x)
+	var sublevels_trees := PackedStringArray()
+	for sub : Node in x.get_children():
+		sublevels_trees.append(_dump_tree_helper(sub, level + 1))
+	var sublevels := "".join(sublevels_trees)
+	return info + "\n" + sublevels
+static func dump_tree(node: Node) -> void:
+	print(_dump_tree_helper(node, 0))
+static func dump_scenetree() -> void:
+	dump_tree(App.get_tree().root)
