612fdb7bd15b9a73262753b7cab07426b80ca834
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-19 08:32:08 -0600

    added abstract debug stub

--- scenes/util/debug.gd
+++ scenes/util/debug.gd
@@ -8,11 +8,17 @@ class_name Debug
 # TODO: When method overloading is added, consolidate the following.
 # Or keep it the same for more type safety?
 static func obj_info(obj: Object) -> String:
-	return obj.get_script().get_path() + "(%s)" % obj.get_class() if obj.get_script() != null else obj.get_class()
+	return obj.get_class() + "(%s)" % obj.get_script().get_path() if obj.get_script() != null else obj.get_class()
 static func node_info(node: Node) -> String:
 	return obj_info(node)
 static func res_info(res: Resource) -> String:
 	return obj_info(res)
+
+static func abstr_func(obj: Object) -> void:
+	var stack := get_stack()
+	var last_call : Dictionary = stack[1]
+	var func_name := last_call["function"] as String
+	push_error("{0} does not implement method {1}".format([obj_info(obj), func_name]))
 
 const TREE_INDENT = "\t"
 static func _tree_info(node: Node) -> String:
@@ -26,5 +32,3 @@ static func _dump_tree_helper(x: Node, level : int) -> String:
 	return info + "\n" + sublevels
 static func dump_tree(node: Node) -> void:
 	print(_dump_tree_helper(node, 0))
-static func dump_scenetree() -> void:
-	dump_tree(App.get_tree().root)
