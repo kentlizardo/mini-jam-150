9baa3e427692c2448bd0b3953d2d5f471cef5b28
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-21 02:43:36 -0600

    Added fake lights

---
+++ scenes/auto/light_helper.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var terrain_shader := load("res://res/terrain_shader.tres") as ShaderMaterial
+
+var dirty := false
+var lights : Array[FakeLight] = []
+
+func add_light(light: FakeLight) -> void:
+	lights.push_back(light)
+
+func remove_light(light: FakeLight) -> bool:
+	var i := lights.find(light)
+	if i != -1:
+		lights.remove_at(i)
+		return true
+	return false
+
+func set_dirty() -> void:
+	dirty = true
+
+const SPHERES = "spheres"
+const SPHERE_COUNT = "sphere_count"
+func _process(delta: float) -> void:
+	if dirty:
+		terrain_shader.set_shader_parameter(SPHERE_COUNT, lights.size())
+		var vecs := PackedVector3Array(lights.map(convert_light))
+		terrain_shader.set_shader_parameter(SPHERES, vecs)
+		dirty = false
+
+func convert_light(light: FakeLight) -> Vector3:
+	return Vector3(light.global_position.x, light.global_position.z, light.radius)
+
---
+++ scenes/game/fake_light.gd
@@ -0,0 +1,18 @@
+class_name FakeLight extends Node3D
+
+@export var radius := 5
+var last_position : Vector3
+
+func _ready() -> void:
+	last_position = global_position
+
+func _enter_tree() -> void:
+	LightHelper.add_light(self)
+
+func _exit_tree() -> void:
+	LightHelper.remove_light(self)
+
+func _physics_process(delta: float) -> void:
+	if last_position != global_position:
+		LightHelper.set_dirty()
+	last_position = global_position
