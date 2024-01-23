7a35579a106b6b6b25897961df768e1cf31389ce
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-23 16:20:38 -0600

    started implementing gridmap

--- scenes/game/map_generator.gd
+++ scenes/game/map_generator.gd
@@ -11,7 +11,8 @@ var LANTERN := load("res://scenes/game/enemies/lantern.tscn")
 
 const tile_size := 1
 
-@export var map_root : Node3D
+@export var grid_map: GridMap
+#@export var map_root : Node3D
 
 var root_node: BSP_Node
 var paths: Array[Dictionary] = []
@@ -47,24 +48,24 @@ const MAX_PADDING := 2
 func check_padding(pos: Vector2i, padding: Vector4i, leaf: BSP_Node) -> bool:
 	return pos.x <= padding.x or pos.y <= padding.y or pos.x > leaf.bounds.size.x - padding.w - 1 or pos.y > leaf.bounds.size.y - padding.z - 1
 
-func set_tile(tile_pos: Vector2i, scene: PackedScene) -> void:
-	if tiles.has(tile_pos):
-		tiles[tile_pos].queue_free()
-	var tile := scene.instantiate()
-	if tile is TileFloor:
-		tile.entered.connect(set_player_floor.bind(tile_pos))
-	map_root.add_child(tile)
-	tile.position.x = tile_pos.x * 2
-	tile.position.z = tile_pos.y * 2
-	tiles[tile_pos] = tile
-
-func add_entity(tile_pos: Vector2i, scene: PackedScene) -> void:
-	var ent := scene.instantiate() as Node3D
-	tile_entities[tile_pos] = ent
-	map_root.add_child(ent)
-	ent.position.x = tile_pos.x * 2
-	ent.position.z = tile_pos.y * 2
-	ent.position.y = 1
+#func set_tile(tile_pos: Vector2i, scene: PackedScene) -> void:
+	#if tiles.has(tile_pos):
+		#tiles[tile_pos].queue_free()
+	#var tile := scene.instantiate()
+	##if tile is TileFloor:
+		##tile.entered.connect(set_player_floor.bind(tile_pos))
+	#map_root.add_child(tile)
+	#tile.position.x = tile_pos.x * 2
+	#tile.position.z = tile_pos.y * 2
+	#tiles[tile_pos] = tile
+#
+#func add_entity(tile_pos: Vector2i, scene: PackedScene) -> void:
+	#var ent := scene.instantiate() as Node3D
+	#tile_entities[tile_pos] = ent
+	#map_root.add_child(ent)
+	#ent.position.x = tile_pos.x * 2
+	#ent.position.z = tile_pos.y * 2
+	#ent.position.y = 1
 
 func set_player_floor(pos: Vector2i) -> void:
 	player_marker = pos
@@ -85,58 +86,61 @@ func build() -> void:
 		for y in range(leaf.bounds.size.y):
 			for x in range(leaf.bounds.size.x):
 				var tile_pos := leaf.bounds.position + Vector2i(x,y)
+				var tile_pos3 := Vector3i(tile_pos.x, 0, tile_pos.y)
 				if check_padding(Vector2i(x, y), padding, leaf):
-					set_tile(tile_pos, WALL)
+					#set_tile(tile_pos, WALL)
+					grid_map.set_cell_item(tile_pos3, 1)
 				else:
-					set_tile(tile_pos, FLOOR)
-					floor_pos_to_room[tile_pos] = leaf
-					leaf.data["unoccupied"].append(tile_pos)
-		for i in range(0, randi_range(MONSTERS_PER_ROOM.x, MONSTERS_PER_ROOM.y)):
-			if leaf.data["unoccupied"].size() > 0:
-				var space := leaf.data["unoccupied"].pick_random() as Vector2i
-				add_entity(space, MONSTER_TABLE.pick_random())
-				leaf.data["unoccupied"].remove_at(leaf.data["unoccupied"].find(space))
-		for i in range(randi_range(0, 4)):
-			if leaf.data["unoccupied"].size() > 0:
-				var space := leaf.data["unoccupied"].pick_random() as Vector2i
-				add_entity(space, LANTERN)
-				leaf.data["unoccupied"].remove_at(leaf.data["unoccupied"].find(space))
-	for path: Dictionary in paths:
-		if path["left"].y == path["right"].y:
-			for i in range(path["right"].x - path["left"].x):
-				var tile_pos := Vector2i(path["left"].x + i, path["left"].y)
-				set_tile(tile_pos, FLOOR)
-		else:
-			for i in range(path["right"].y - path["left"].y):
-				var tile_pos := Vector2i(path["left"].x, path["left"].y + i)
-				set_tile(tile_pos, FLOOR)
-	var ladder_spawns := []
-	var player_spawns := []
-	for y in range(20):
-		for x in range(root_node.bounds.size.x):
-			var tile_pos := Vector2i(root_node.bounds.position.x + x, root_node.bounds.position.y + y)
-			if tiles.has(tile_pos):
-				if tiles[tile_pos] is TileFloor and !tile_entities.keys().has(tile_pos):
-					ladder_spawns.push_back(tile_pos)
-	for y in range(20):
-		for x in range(root_node.bounds.size.x):
-			var tile_pos := Vector2i(root_node.bounds.position.x + x, root_node.bounds.position.y + root_node.bounds.size.y - y)
-			if tiles.has(tile_pos):
-				if tiles[tile_pos] is TileFloor and !tile_entities.keys().has(tile_pos):
-					player_spawns.push_back(tile_pos)
-	await get_tree().process_frame
-	for leaf in root_node.get_leaves():
-		leaf.entered = false
-	var p_spawn := player_spawns.pick_random() as Vector2i
-	var player := Player.current_player
-	player.position.x = p_spawn.x * 2
-	player.position.z = p_spawn.y * 2
-	player.position.y = 1
-	var l_spawn := ladder_spawns.pick_random() as Vector2i
-	add_entity(l_spawn, load("res://scenes/game/enemies/ladder.tscn"))
-	for i: Node3D in tile_entities.values():
-		if i.global_position.distance_to(player.global_position) <= 20:
-			i.queue_free()
+					#set_tile(tile_pos, FLOOR)
+					grid_map.set_cell_item(tile_pos3, 0)
+					#floor_pos_to_room[tile_pos] = leaf
+					#leaf.data["unoccupied"].append(tile_pos)
+		#for i in range(0, randi_range(MONSTERS_PER_ROOM.x, MONSTERS_PER_ROOM.y)):
+			#if leaf.data["unoccupied"].size() > 0:
+				#var space := leaf.data["unoccupied"].pick_random() as Vector2i
+				#add_entity(space, MONSTER_TABLE.pick_random())
+				#leaf.data["unoccupied"].remove_at(leaf.data["unoccupied"].find(space))
+		#for i in range(randi_range(0, 4)):
+			#if leaf.data["unoccupied"].size() > 0:
+				#var space := leaf.data["unoccupied"].pick_random() as Vector2i
+				#add_entity(space, LANTERN)
+				#leaf.data["unoccupied"].remove_at(leaf.data["unoccupied"].find(space))
+	#for path: Dictionary in paths:
+		#if path["left"].y == path["right"].y:
+			#for i in range(path["right"].x - path["left"].x):
+				#var tile_pos := Vector2i(path["left"].x + i, path["left"].y)
+				#set_tile(tile_pos, FLOOR)
+		#else:
+			#for i in range(path["right"].y - path["left"].y):
+				#var tile_pos := Vector2i(path["left"].x, path["left"].y + i)
+				#set_tile(tile_pos, FLOOR)
+	#var ladder_spawns := []
+	#var player_spawns := []
+	#for y in range(20):
+		#for x in range(root_node.bounds.size.x):
+			#var tile_pos := Vector2i(root_node.bounds.position.x + x, root_node.bounds.position.y + y)
+			#if tiles.has(tile_pos):
+				#if tiles[tile_pos] is TileFloor and !tile_entities.keys().has(tile_pos):
+					#ladder_spawns.push_back(tile_pos)
+	#for y in range(20):
+		#for x in range(root_node.bounds.size.x):
+			#var tile_pos := Vector2i(root_node.bounds.position.x + x, root_node.bounds.position.y + root_node.bounds.size.y - y)
+			#if tiles.has(tile_pos):
+				#if tiles[tile_pos] is TileFloor and !tile_entities.keys().has(tile_pos):
+					#player_spawns.push_back(tile_pos)
+	#await get_tree().process_frame
+	#for leaf in root_node.get_leaves():
+		#leaf.entered = false
+	#var p_spawn := player_spawns.pick_random() as Vector2i
+	#var player := Player.current_player
+	#player.position.x = p_spawn.x * 2
+	#player.position.z = p_spawn.y * 2
+	#player.position.y = 1
+	#var l_spawn := ladder_spawns.pick_random() as Vector2i
+	#add_entity(l_spawn, load("res://scenes/game/enemies/ladder.tscn"))
+	#for i: Node3D in tile_entities.values():
+		#if i.global_position.distance_to(player.global_position) <= 20:
+			#i.queue_free()
 
 # References:
 #https://jonoshields.com/post/bsp-dungeon
